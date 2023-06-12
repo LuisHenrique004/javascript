8d9e80402db17164c64e833b1d9b67a15f753f32
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-06-12 11:27:12 -0300

    Praticando o que foi aprendido

---
+++ pratica/variavel.js
@@ -0,0 +1,37 @@
+/*
+pode conter acento e simbolos
+não pode ter espaçamento, a não ser que utilize '-' '_' para criar o espaçamento
+não pode utilizar palavras reservadas.
+sempre tentar utilizar nome coerentes
+a variavel não pode começar com número, mas pode conter número no meio dela
+pode-se utilizar 'let' ou 'var' para criar uma variavel
+
+Obs: CTRL + L limpa o terminal do node.js
+*/
+
+/*
+TIPOS PRIMITIVOS DE DADOS
+• NUMBER, STRING, BOOLEAN
+O NUMBER PODE SER DO TIPO INFINITY E NAN (NOT A NUMBER)
+• NULL, UNDEFINED, OBJECT -> (O OBJECT INCLUI O ARRAY)
+• FUNCTION
+
+o comando typeof indica qual o tipo de cada elemento.
+
+*/
+
+var a = 8
+console.log(a)
+let b = 9
+console.log(b)
+
+console.log(typeof 8)
+console.log(typeof 'oi')
+console.log(typeof true)
+console.log(typeof false)
+console.log(typeof 8*1)
+console.log(typeof null)
+console.log(typeof array)
+console.log(typeof Math.sin)
+
+
