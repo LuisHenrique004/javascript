c14c145cb11f364075e6e4db85853c55e296c6f3
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-06-13 00:23:24 -0300

    condições em JavaScript

---
+++ pratica/condicoes.js
@@ -0,0 +1,62 @@
+/*  • Condições simples
+    Condicações simples são aquelas que contem apenas um if
+    
+    if (condicao) {
+        True
+    }
+
+*/
+
+/* • Condições compostas
+    Condições compostas são formadas por if e else
+
+    if (condicao) {
+        True
+    } else {
+        False
+    }
+
+*/
+
+/* • Condiçõies aninhadas
+    São condições dentro de outra condições
+
+    if (condicao) {
+        bloco1
+    } else {
+        if (condicao) {
+            bloco2
+        } else {
+            bloco3
+        }
+    }
+
+            OU
+
+    if (condicao) {
+        true
+    } else if {
+        true
+    } else {
+        false
+    }
+
+*/
+
+/* • Condições Multiplas
+    Utilizada para valores fixos
+
+    switch(expressao) {
+        case 1:
+            bloco1
+            break
+        case 2:
+            bloco2
+            break
+        default:
+            bloco padrão
+            break
+    }
+
+    Tem que ter o break
+*/
