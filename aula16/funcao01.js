4f894ce690b9d9488e211e51cf7ac5532d0a345d
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-06-20 11:55:08 -0300

    Funções em JavaScript

---
+++ aula16/funcao01.js
@@ -0,0 +1,10 @@
+function parimp(n) {
+    if (n%2 == 0) {
+        return console.log(`o valor ${n} é um número par`)
+        
+    } else {
+        return console.log(`o valor de ${n} é impar`)
+    }
+} 
+
+parimp(10)
---
+++ aula16/funcao02.js
@@ -0,0 +1,6 @@
+function soma(n1=0, n2=0) {
+    let res = (n1 + n2)
+    return console.log(`A soma entre ${n1} e ${n2} é igual a: ${res} :)`)
+}
+
+soma(2, 10)
