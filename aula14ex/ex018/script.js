2fe701417d62a28085a4a7347d7acf8fdc247061
Luis Henrique <75598800+LuisHenrique004@users.noreply.github.com>
2023-06-21 23:41:56 -0300

    Terminando a atividade

--- aula14ex/ex018/script.js
+++ aula14ex/ex018/script.js
@@ -1,17 +1,50 @@
 let txtn = document.getElementById('inum')
 let res = window.document.getElementById('res')
 let lista = []
+let soma = 0
+let media = 0
+let maior = 0
+let menor = 0
+
 
 
 function adicionar() {
     let n = Number(txtn.value)
-    lista.push(n)
-    res.innerHTML += `<option>Valor ${n} adicionado</option>`
+
+    //regra para adicionar número
+    if(txtn.value.length == '' || n < 1 || n > 100) {
+        alert('[Erro] Erro ao informar o valor')
+    } else {
+        lista.push(n)
+        res.innerHTML += `<option>Valor ${n} adicionado</option>`
+        soma += n
+        media = soma / 2
+        
+        //Calcular maior número
+        for (let pos in lista) {
+            if(lista[pos] > maior) {
+                maior = lista[pos]
+            }
+        }
+
+        //Calcular menor número
+        menor = maior
+        for(let i = 0; i < lista.length; i++) {
+            if(lista[i] < menor) {
+                menor = lista[i]
+            }
+        }
+    }
+
 }
 
 
 function calcular() {
     let msg = window.document.getElementById('calc')
-    msg.innerHTML = `Ao todo, temos ${lista.length} números cadastrados. <br>`
+    msg.innerHTML = `Ao todo, temos ${lista.length} números cadastrados. <br>
+    O maior valor informado foi ${maior} <br>
+    O menor valor informado foi ${menor} <br>
+    Somando todos os valores, temos ${soma} <br>
+    A média dos valores digitados é ${media}`
 }
 
